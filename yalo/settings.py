7567c256f5149581354166a5bb9bd69e7b03ce37
Jorge Limas <jorge.limas@yalo.com>
2022-06-02 17:49:21 -0600

    Setup static files directory

--- yalo/settings.py
+++ yalo/settings.py
@@ -142,6 +142,8 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = "/var/www/yalo-conecta-api/static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
