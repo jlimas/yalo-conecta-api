35946863949eaabf956410dd3c311f4a5cb07b00
Jorge Limas <jorge.limas@yalo.com>
2022-06-03 09:30:20 -0600

    Center the logo for the event on main page

--- yalo/settings.py
+++ yalo/settings.py
@@ -144,6 +144,8 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = "/var/www/yalo-conecta-api/static"
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- yalo/urls.py
+++ yalo/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
@@ -21,4 +23,4 @@ urlpatterns = [
     path("boletos", html_views.boletos),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
